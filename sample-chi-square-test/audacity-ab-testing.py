c7cbb20b964d549dff3a075645602bcccdf7c98d
kokeshiM0chi <frogandfrog009@gmail.com>
2024-09-24 10:52:49 +0900

    feat: add a chi square test

---
+++ sample-chi-square-test/audacity-ab-testing.py
@@ -0,0 +1,18 @@
+import math
+import numpy as np
+import pandas as pd
+import scipy.stats
+
+df=pd.read_csv("./data/homepage_actions.csv")
+print(df.head())
+
+group=df.groupby('group').count()
+print(group)
+
+data=np.matrix([[932,3332],[928,2996]])
+chi2,p,ddof,expected=scipy.stats.chi2_contingency(data,correction=False)
+
+print("カイ二乗値:", chi2)
+print("p値:", p)
+print("自由度:", ddof)
+print("期待度数:", expected)
